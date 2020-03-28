1eba98c15bcec56602ebfc437fd2ef303aaeb4a1
Weirenlan <miblue119@gmail.com>
2020-03-28 16:53:50 +0800

    Initial Commit

---
+++ LeetCode_Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LeetCode_Practice
+//
+//  Created by weiren Lan on 2020/3/28.
+//  Copyright © 2020年 weiren Lan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
